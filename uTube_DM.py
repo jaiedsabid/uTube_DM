f1b300a527f8ca5e439c1d260a1e32f05279dc58
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2020-08-06 16:52:45 +0600

    The operating system detection has been added, and the file save path will change based on that. And last, few bugs got fixed.

--- uTube_DM.py
+++ uTube_DM.py
@@ -1,16 +1,38 @@
 import os
 import re
+import platform
 
 from pytube import YouTube
 
 
+def checkWindowsDrive(DriveLetter: str) -> bool:
+    '''
+    Check if windows drive letter is correct or not.
+    '''
+    DriveLetter = DriveLetter + ':\\'
+    return os.path.isdir(DriveLetter)
+
+
 def main():
     vRes_pat = r'res=\"([\d\w]+)\"'
-    print('---------------------------------------------------')
-    print('Enter the drive letter where video will be saved')
-    print('---------------------------------------------------')
-    save_path = '<DRIVE_LETTER>:\\uTube_DM'.replace('<DRIVE_LETTER>',
-                                                    str(input('=> ').upper()))
+    while(True):
+        if platform.system() == 'Windows':
+            print('---------------------------------------------------')
+            print('Enter the drive letter where video will be saved')
+            print('---------------------------------------------------')
+            drivePath = str(input('=> '))
+            save_path = '<DRIVE_LETTER>:\\uTube_DM'
+            if checkWindowsDrive(drivePath.upper()):
+                save_path = save_path.replace(
+                    '<DRIVE_LETTER>', str(drivePath.upper()))
+                break
+            else:
+                print(
+                    '\nInvalid drive location!\nPlease enter a valid drive location.\n')
+        elif platform.system() == 'Linux':
+            save_path = '/home/<USER>/Downloads'.replace(
+                '<USER>', os.getlogin())
+            break
     while(True):
         vResNvideos = {}
         vRes_list = []
@@ -19,28 +41,33 @@ def main():
         print('---------------------------------------------------\n')
         url_ = str(input('=> '))
         if(url_.lower() != 'exit'):
-            videos_ = YouTube(url=url_).streams.filter(progressive=True)
-            print('\n---------------------------------------------------')
-            print('Available video resolutions')
-            print('---------------------------------------------------\n')
-            opRes = 1
-            for video_ in videos_:
-                vRes = re.findall(vRes_pat, str(video_))
-                vResNvideos[str(vRes[0])] = video_
-                vRes_list.append(str(vRes[0]))
-                print('{0}) {1}'.format(opRes, vRes[0]))
-                opRes += 1
-            print('\n---------------------------------------------------')
-            print('Choose video resulation from the available list\nex. 1 for 720p etc.')
-            print('---------------------------------------------------\n')
-            opRes = int(input('=> '))
-            vResNvideos[vRes_list[opRes-1]].download(save_path)
-            print('\nFile saved at', save_path)
+            try:
+                videos_ = YouTube(url=url_).streams.filter(progressive=True)
+                print('\n---------------------------------------------------')
+                print('Available video resolutions')
+                print('---------------------------------------------------\n')
+                opRes = 1
+                for video_ in videos_:
+                    vRes = re.findall(vRes_pat, str(video_))
+                    vResNvideos[str(vRes[0])] = video_
+                    vRes_list.append(str(vRes[0]))
+                    print('{0}) {1}'.format(opRes, vRes[0]))
+                    opRes += 1
+                print('\n---------------------------------------------------')
+                print(
+                    'Choose video resulation from the available list\nex. 1 for 720p etc.')
+                print('---------------------------------------------------\n')
+                try:
+                    opRes = int(input('=> '))
+                    vResNvideos[vRes_list[opRes-1]].download(save_path)
+                    print('\nFile saved at', save_path)
+                except ValueError as errorCode:
+                    print('\nInvalid input!\nReturned to main menu.')
+            except Exception:
+                print('\nInvalid URL!')
         elif(url_.lower() == 'exit'):
-            print('Exit successfully!')
+            print('\nExit successfully!')
             break
-        else:
-            print('Wrong input.')
 
 
 if __name__ == '__main__':
